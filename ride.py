0172bae3bf622c12511e532db38d981aa7bd25b4
ShauryadityaSingh-art <kaachabadam0801@gmail.com>
2026-08-12 20:45:28 +0530

    Add files via upload

---
+++ ride.py
@@ -0,0 +1,59 @@
+print("=============================================")
+print("          Welcome to Ride Builder"            )
+print("=============================================")
+print()
+
+print("Step 1: Pick your vehicle")
+print("  1 - Bike")
+print("  2 - Car")
+print()
+
+choice = int(input("Enter 1 or 2: "))
+print()
+
+if choice == 1:
+    print("Step 2: Pick your bike type")
+    print("  1 - Scooty")
+    print("  2 - Mountain Bike")
+    print()
+
+    bike_type = int(input("Enter 1 or 2: "))
+    print()
+
+    if bike_type == 1:
+        print("You Picked  : Scooty")
+        print("Top Speed   : 80 km/h")
+        print("Best for    : City roads")
+    else:
+        print("You picked  : Mountain Bike")
+        print("Top Speed   : 40 km/h")
+        print("Best for    : Off-road trails")
+
+elif choice == 2:
+    print("Step 2: Pick your car type")
+    print("  1 - Sedan")
+    print("  2 - SUV")
+    print()
+
+    car_type = int(input("Enter 1 or 2: "))
+    print()
+
+    if car_type == 1:
+        print("Your picked  : Sedan")
+        print("Seats        : 5 passengers")
+        print("Best for     : Family Trips")
+    else:
+        print("You picked  : SUV")
+        print("Seats       : 7 passengers")
+        print("Best for    : Off-road adventures")
+
+else:
+    print("That was not a valid choice.")
+    print("Plese enter1 for bike and 2 for car.")
+
+print()
+print("=================================")
+print("    Your custom ride is ready!   ")
+print("       Enjoy your Journey!       ")
+print("=================================")
+ 
